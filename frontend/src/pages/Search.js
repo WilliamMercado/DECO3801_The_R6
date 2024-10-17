2c3557cfcff5d0f20eb013f7614e08c2ab70d1de
Wotah Man <z.p.a.toto@gmail.com>
2024-10-17 22:36:17 +1000

    changed base url to ngrok hosted endpoint

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -13,7 +13,7 @@ export default function Search() {
     const [loading, setLoading] = useState(false); // Show or hide a loading indicator during searches
     const [savedImages, setSavedImages] = useState(new Set()); // Stores images that have been saved/bookmarked
     const [showSearchHistory, setShowSearchHistory] = useState(1); // Controls whether the search history is visible
-    const baseUrl = 'http://127.0.0.1:5000'; // Define the base URL for the backend API requests
+    const baseUrl = ' https://c35d7ca285b3.ngrok.app'; // Define the base URL for the backend API requests
     
     useEffect(() => {
         const id = localStorage.getItem('userId');
