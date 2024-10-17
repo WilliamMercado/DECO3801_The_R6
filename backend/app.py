f7b094b12950fa38ef26c6a8c242d45107d2146a
Wotah Man <z.p.a.toto@gmail.com>
2024-10-18 08:18:56 +1000

    fixed wrong url

--- backend/app.py
+++ backend/app.py
@@ -54,4 +54,4 @@ if __name__ == '__main__':
 
     with app.app_context():
 
-        app.run(host='127.0.0.1', port=5000, debug=True)
+        app.run(host='0.0.0.0', port=5000, debug=True)
--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -33,7 +33,7 @@ export default function Upload() {
     //for ai using
     const [generatedImages, setGeneratedImages] = useState({}); // State to store AI-generated image filenames
     const [savedGeneratedImages, setSavedGeneratedImages] = useState(new Set()); // Set of saved image names
-    const baseUrl = '127.0.0.1:5000';
+    const baseUrl = 'http://127.0.0.1:5000';
     
     useEffect(() => {
         const id = localStorage.getItem('userId');
